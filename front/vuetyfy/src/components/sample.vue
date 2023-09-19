14aed3d790dd9449be3830489baacd4e7ee18eff
takuya-iwanaga <nishi.iwa0901@gmail.com>
2023-09-19 22:29:15 +0900

    ヘッダーと変数入力の仕様完了

--- front/vuetyfy/src/components/sample.vue
+++ front/vuetyfy/src/components/sample.vue
@@ -2,15 +2,19 @@
   
     
     <v-app-bar flat>
-      <v-app-bar-title>
-        <v-icon icon="mdi-circle-slice-4" />
-        Base Preset
-      </v-app-bar-title>
+      
+        <svg-icon type="mdi" :path="mdiMenu"></svg-icon>
+        <v-toolbar-title class="text-center">{{message}}</v-toolbar-title>
+      
     </v-app-bar>
     
     
   </template>
   
   <script setup>
-    //
+    import SvgIcon from '@jamescoyle/vue-icon';
+    import { mdiMenu } from '@mdi/js';
+    
+    const message = 'すやま歯科医院'
+    
   </script>
