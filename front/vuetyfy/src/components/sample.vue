a22fd4bfc115544d3722de320045e23291f152e0
takuya-iwanaga <nishi.iwa0901@gmail.com>
2023-09-21 18:44:25 +0900

    サイドバー調整中20230921

--- front/vuetyfy/src/components/sample.vue
+++ front/vuetyfy/src/components/sample.vue
@@ -1,12 +1,6 @@
 <template>
 
-  <v-app-bar flat>
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-    <v-toolbar-title class="text-center ml-n7">{{message}}</v-toolbar-title>
-
-  </v-app-bar>
-
+  <!--サイドバーの表示-->
   <v-navigation-drawer
       v-model="drawer"
       absolute
@@ -19,32 +13,50 @@
       >
         <v-list-item-group
           v-model="group"
-          active-class="deep-purple--text text--accent-4"
+          active-class=""
         >
-          <v-list-item>
-            <v-list-item-title>Foo</v-list-item-title>
-          </v-list-item>
+        <img src='@/assets/suyamasika.jpg'
+        >
 
-          <v-list-item>
-            <v-list-item-title>Bar</v-list-item-title>
-          </v-list-item>
+        <div>
+        <svg-icon type="mdi" :path="mdiInformation"></svg-icon>
+        医院情報
+        </div>
+        <div>
+        <svg-icon type="mdi" :path="mdiPhone"></svg-icon>
+        電話をかける
+        </div>
+        <div>
+        <svg-icon type="mdi" :path="mdiFileDocumentOutline"></svg-icon>
+        利用規約
+        </div>
+        <div>
+        <svg-icon type="mdi" :path="mdiShield"></svg-icon>
+        プライバシーポリシー
+        </div>
 
-          <v-list-item>
-            <v-list-item-title>Fizz</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Buzz</v-list-item-title>
-          </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
+
+    <v-app-bar flat>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-toolbar-title class="text-center ml-n7">{{message}}</v-toolbar-title>
+
+  </v-app-bar>
+
     
   </template>
   
   <script setup>
+  //アイコンの使用
     import SvgIcon from '@jamescoyle/vue-icon';
-    import { mdiMenu } from '@mdi/js';
+    import { mdiInformation } from '@mdi/js';
+    import { mdiPhone } from '@mdi/js';
+    import { mdiFileDocumentOutline } from '@mdi/js';
+    import { mdiShield } from '@mdi/js';
+
     import { ref, watch } from 'vue'
 
     const message = 'すやま歯科医院'
