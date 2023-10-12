01d054363573112a02e017f1071c7e6fb52e401b
ganchan <43211384+takuya-iwanaga@users.noreply.github.com>
2023-10-12 19:09:40 +0900

    Delete front directory
    間違えたので消します

--- front/vuetyfy/src/components/bottompushview/reserve.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <Header title="ご予約"/>
-    <div>Reserve</div>
-    </template>
-    <script setup>
-    import Header from '../Header.vue';
-    </script>
--- front/vuetyfy/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    devServer: {
-      proxy: {
-        "/user/": {
-          target: "http://localhost:4050",
-        }
-      }
-    }
-  };
